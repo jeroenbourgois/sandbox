e63d2844d43ec882c09776718ca75cceafac1ea3
Jeroen Bourgois <jeroenbourgois@gmail.com>
2011-10-10 22:07:53 +0200

    fresh app

---
+++ padaboom/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Padaboom::Application.config.secret_token = 'a1214210cf3f82c48b08468d18f02eaa212708b94f6d22e22668f54ce90c6dec3d5be3795bc6b0f597486e5d7afe3a81d78b6ac2366833439a7c5d33118c2000'
